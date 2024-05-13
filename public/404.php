1779a5e5f4884e64c0f7720552d73771510bfe06
Kami <rokugaki.sama@gmail.com>
2024-05-13 19:07:56 +0200

    404 Creation + Contact css

--- public/404.php
+++ public/404.php
@@ -1,2 +1,4 @@
-
-
+<div class="container">
+    <h1>404 - Page not found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+</div>
--- public/hero.php
+++ public/hero.php
@@ -1,4 +1,4 @@
-<div class="hero">
+<div class="hero landing">
     <h1>Welcome to WildCampers!</h1>
     <p>Your Ultimate Adventure Companion!</p>
     <a href="learn_more.php" class="btn">Learn More</a>
